96f270765c573033e4eac9357a37e4017121128c
MaryDivya <divya.pentareddy@gmail.com>
2022-05-28 10:01:08 +1000

    third commit

--- src/test/java/POMTests/TC01_GooglePagrTest.java
+++ src/test/java/POMTests/TC01_GooglePagrTest.java
@@ -1,39 +1,39 @@
-package POMTests;
-
-import BaseClass.BrowserAction;
-import Pages.LoginPage;
-import org.openqa.selenium.Alert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-public class TC01_GooglePagrTest {
-
-    @BeforeTest
-    public void startingBrowser()
-    {
-        BrowserAction Action=new BrowserAction();
-        Action.OpenBrowserType("chrome");
-        Action.getUrl("https://www.google.com/?hl=en-US&authuser=1");
-    }
-
-
-    @Test
-    public void Googlepage()
-    {
-        LoginPage log= new LoginPage(BrowserAction.driver);
-        log.TextBox("Divya");
-        log.GmaiLinkClick();
-
-
-    }
-    @AfterTest
-    public void Teardown()
-    {
-        BrowserAction Action=new BrowserAction();
-        Action.ClosingBrowser();
-    }
-
-
-}
+//package POMTests;
+//
+//import BaseClass.BrowserAction;
+//import Pages.LoginPage;
+//import org.openqa.selenium.Alert;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
+//
+//public class TC01_GooglePagrTest {
+//
+//    @BeforeTest
+//    public void startingBrowser()
+//    {
+//        BrowserAction Action=new BrowserAction();
+//        Action.OpenBrowserType("chrome");
+//        Action.getUrl("https://www.google.com/?hl=en-US&authuser=1");
+//    }
+//
+//
+//    @Test
+//    public void Googlepage()
+//    {
+//        LoginPage log= new LoginPage(BrowserAction.driver);
+//        log.TextBox("Divya");
+//        log.GmaiLinkClick();
+//
+//
+//    }
+//    @AfterTest
+//    public void Teardown()
+//    {
+//        BrowserAction Action=new BrowserAction();
+//        Action.ClosingBrowser();
+//    }
+//
+//
+//}
